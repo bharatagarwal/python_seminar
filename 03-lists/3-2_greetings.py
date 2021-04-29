9e9d35b29eda076e97bbb8de95df1c3339b0ca35
Bharat Agarwal <bharatagarwal@gmail.com>
2021-04-30 02:26:31 +0530

    Starting with exercises about accessing lists

---
+++ 03-lists/3-2_greetings.py
@@ -0,0 +1,6 @@
+names = ['Shivam', 'Aneesh', 'Catherine', 'Markus']
+
+print(f"{names[0]}, it's been a while.")
+print(f"{names[1]}, I'm not even sure we'll get along now")
+print(f"{names[2]}, it's been amazing knowing you")
+print(f"{names[3]}, you're an amazing headstrong creative guy!")
